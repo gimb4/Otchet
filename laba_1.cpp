da7c49f046f17f1f6b7ec825599ab8217ef42667
rr <gregrger@gmail.com>
2023-11-07 14:44:57 +0300

    Zd1-4

---
+++ laba_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int number1, number2, answer, correctAnswer;
+    cout << "Введите первое число";
+    cin >> number1;
+
+    cout << "Введите второе число";
+    cin >> number2;
+
+    correctAnswer = number1 * number2;
+
+    cout << "Сколько будет" << number1 << "умножить на" << number2 << "?";
+    cin >> answer;
+
+    if (answer == correctAnswer) {
+        cout << "Правильно!" << endl;
+    
+    }
+    else {
+
+        cout << "Неправильно. Правильный ответ: " << correctAnswer << endl;
+    }
+    return 0;
+}                                       
+
+
---
+++ laba_4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+bool isPasswordValid(const std::string& password) {
+    bool hasUpperCase = false;
+    bool hasLowerCase = false;
+    bool hasSpecialChar = false;
+
+    for (char c : password) {
+        if (isupper(c)) {
+            hasUpperCase = true;
+        }
+        else if (islower(c)) {
+            hasLowerCase = true;
+        }
+        else if (c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || c == '&' || c == '*' || c == '+' || c == '?') {
+            hasSpecialChar = true;
+        }
+    }
+
+    return hasUpperCase && hasLowerCase && hasSpecialChar;
+}
+
+std::string generatePassword(int length) {
+    std::string password;
+    std::string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*+?";
+
+    srand(time(0));
+
+    for (int i = 0; i < length; i++) {
+        int randomIndex = rand() % characters.length();
+        password += characters[randomIndex];
+    }
+
+    return password;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int passwordLength;
+    std::cout << "Введите длину пароля: ";
+    std::cin >> passwordLength;
+
+    std::string password = generatePassword(passwordLength);
+    std::cout << "Сгенерированный пароль: " << password << std::endl;
+
+    if (isPasswordValid(password)) {
+        std::cout << "Пароль действителен." << std::endl;
+    }
+    else {
+        std::cout << "Пароль недействителен." << std::endl;
+    }
+
+    return 0;
+}
